95c2fce8431043eeeb342baaf8586da92cb032a0
Ben Reid <reidben24@gmail.com>
2021-04-21 15:20:37 -0400

    Log config when all loaded

--- src/Core/Properties.cpp
+++ src/Core/Properties.cpp
@@ -83,6 +83,7 @@ bool Properties::load() {
     if (!bl::engine::Configuration::load("configuration.cfg")) {
         BL_LOG_INFO << "Failed to load configuration file, using defaults";
     }
+    bl::engine::Configuration::log();
 
     menuFont = bl::engine::Resources::fonts()
                    .load(bl::engine::Configuration::get<std::string>("core.menu.primary_font"))
