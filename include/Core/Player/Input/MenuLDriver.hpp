eeb2fd512210f0d82d0ca58aa7a1d99ec7fedb6b
Ben Reid <reidben24@gmail.com>
2021-05-04 23:38:45 -0400

    Begin implementing player input driven menu controller

---
+++ include/Core/Player/Input/MenuLDriver.hpp
@@ -0,0 +1,24 @@
+#ifndef CORE_PLAYER_INPUT_MENUDRIVER_HPP
+#define CORE_PLAYER_INPUT_MENUDRIVER_HPP
+
+#include <BLIB/Menu.hpp>
+
+namespace core
+{
+namespace player
+{
+namespace input
+{
+class MenuDriver {
+public:
+    //
+
+private:
+    //
+};
+
+} // namespace input
+} // namespace player
+} // namespace core
+
+#endif
