2e4df86284c333f0b3d017ef03201ef6d11b288b
Ben Reid <reidben24@gmail.com>
2021-05-29 00:57:22 -0400

    Add Direction to/from string methods. Implement script getPlayer and giveItem

--- src/Core/Components/Direction.cpp
+++ src/Core/Components/Direction.cpp
@@ -29,5 +29,31 @@ Direction nextCounterClockwiseDirection(Direction dir) {
     }
 }
 
+Direction directionFromString(const std::string& str) {
+    switch (str.empty() ? 'u' : str[0]) {
+    case 'u':
+        return Direction::Up;
+    case 'r':
+        return Direction::Right;
+    case 'd':
+        return Direction::Down;
+    default:
+        return Direction::Left;
+    }
+}
+
+std::string directionToString(Direction dir) {
+    switch (dir) {
+    case Direction::Up:
+        return "up";
+    case Direction::Right:
+        return "right";
+    case Direction::Down:
+        return "down";
+    default:
+        return "left";
+    }
+}
+
 } // namespace component
 } // namespace core
--- src/Core/Scripts/Functions.cpp
+++ src/Core/Scripts/Functions.cpp
@@ -1,11 +1,14 @@
 #include <Core/Scripts/Functions.hpp>
 
+#include <BLIB/Scripts.hpp>
+#include <BLIB/Util/Waiter.hpp>
 #include <Core/Systems/Systems.hpp>
 
 namespace core
 {
 namespace script
 {
+using bl::script::Error;
 using bl::script::SymbolTable;
 using bl::script::Value;
 using Function = bl::script::Function::CustomCB;
@@ -99,14 +102,92 @@ void Functions::addDefaults(SymbolTable& table, system::Systems& systems) {
 
 namespace
 {
+Value makePosition(system::Systems& systems, bl::entity::Entity e) {
+    Value value;
+    const component::Position* pos =
+        systems.engine().entities().getComponent<component::Position>(e);
+    if (pos) {
+        Value coord;
+        coord.setProperty("x", {static_cast<float>(pos->positionTiles().x)});
+        coord.setProperty("y", {static_cast<float>(pos->positionTiles().y)});
+        value.setProperty("tiles", coord);
+        coord.setProperty("x", {pos->positionPixels().x});
+        coord.setProperty("y", {pos->positionPixels().y});
+        value.setProperty("pixels", coord);
+        value.setProperty("level", {static_cast<float>(pos->level)});
+        value.setProperty("direction", {component::directionToString(pos->direction)});
+    }
+    return value;
+}
+
+template<Value::Type... Types>
+void validateArgs(const std::string& func, ArgList args) {
+    const Value::Type types[] = {Types...};
+    const unsigned int nargs  = sizeof...(Types);
+
+    if (args.size() != nargs)
+        throw Error(func + "() takes " + std::to_string(nargs) + " arguments");
+    for (unsigned int i = 0; i < nargs; ++i) {
+        if (args[i].getType() != types[i]) {
+            throw Error(func + "() argument " + std::to_string(i) + " must be a " +
+                        Value::typeToString(types[i]) + " but " +
+                        Value::typeToString(args[i].getType()) + " was passed");
+        }
+    }
+}
+
 Value getPlayer(system::Systems& systems, ArgList) {
-    // TODO
-    return makeBool(false);
+    Value player(static_cast<float>(systems.player().player()));
+    player.setProperty("name", {"player name"}); // TODO - store player name
+    player.setProperty("gender", {"?"});         // TODO - store player gender
+    player.setProperty("money", {100.f});        // TODO - track player money
+    player.setProperty("position", makePosition(systems, systems.player().player()));
+
+    std::vector<player::Bag::Item> items;
+    systems.player().bag().getAll(items);
+    std::vector<Value::Ptr> bag;
+    bag.reserve(items.size());
+    for (const player::Bag::Item& item : items) {
+        bag.emplace_back(new Value(static_cast<float>(item.id)));
+        bag.back()->setProperty("id", {static_cast<float>(item.id)});
+        bag.back()->setProperty("name", {item::Item::getName(item.id)});
+        bag.back()->setProperty("qty", {static_cast<float>(item.qty)});
+    }
+    player.setProperty("bag", {bag});
+
+    return player;
 }
 
 Value giveItem(system::Systems& systems, ArgList args) {
-    // TODO
-    return makeBool(false);
+    validateArgs<Value::TNumeric, Value::TNumeric, Value::TBool, Value::TBool>("giveItem", args);
+
+    const unsigned int rawId = static_cast<unsigned int>(args[0].getAsNum());
+    const item::Id item      = item::Item::cast(rawId);
+    if (item != item::Id::Unknown) {
+        const int qty = static_cast<int>(args[1].getAsNum());
+        if (qty > 0) {
+            systems.player().bag().addItem(item, qty);
+            if (args[2].getAsBool()) {
+                bl::util::Waiter waiter;
+                system::HUD::Callback unlock = [](const std::string&) {};
+                if (args[3].getAsBool())
+                    unlock = [&waiter](const std::string&) { waiter.unblock(); };
+
+                const std::string msg = qty > 1 ? ("Received " + std::to_string(qty) + " " +
+                                                   item::Item::getName(item) + "s") :
+                                                  ("Received a " + item::Item::getName(item));
+                systems.hud().displayMessage(msg, unlock);
+                if (args[3].getAsBool()) waiter.wait();
+            }
+        }
+        else {
+            BL_LOG_WARN << "qty must be a positive integer";
+        }
+    }
+    else {
+        BL_LOG_WARN << "Unknown item id: " << rawId;
+    }
+    return {};
 }
 
 Value giveMoney(system::Systems& systems, ArgList args) {
