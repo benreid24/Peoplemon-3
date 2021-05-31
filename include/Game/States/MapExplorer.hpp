6320bc86cd775964bb88aae753a6078a050a6849
Ben Reid <reidben24@gmail.com>
2021-05-31 19:34:26 -0400

    Document map explorer interface

--- include/Game/States/MapExplorer.hpp
+++ include/Game/States/MapExplorer.hpp
@@ -8,22 +8,64 @@ namespace game
 {
 namespace state
 {
+/**
+ * @brief Debug only state that allows free roaming the map with a special camera
+ *
+ * @ingroup States
+ *
+ */
 class MapExplorer
 : public State
 , public bl::event::Listener<sf::Event> {
 public:
+    /**
+     * @brief Creates a new MapExplorer state
+     *
+     * @param systems The primary systems object
+     * @return bl::engine::State::Ptr The new state
+     */
     static bl::engine::State::Ptr create(core::system::Systems& systems);
 
+    /**
+     * @brief Destroy the Map Explorer state
+     *
+     */
     virtual ~MapExplorer() = default;
 
+    /**
+     * @brief Returns "MapExplorer"
+     *
+     */
     virtual const char* name() const override;
 
+    /**
+     * @brief Activates the state and camera. Discnnects the player from their entity
+     *
+     * @param engine The game engine
+     */
     virtual void activate(bl::engine::Engine& engine) override;
 
+    /**
+     * @brief Reconnects the player to their entity and pops the camera
+     *
+     * @param engine The game engine
+     */
     virtual void deactivate(bl::engine::Engine& engine) override;
 
+    /**
+     * @brief Updates the game world logic and the hint fadeout
+     *
+     * @param engine The game engine
+     * @param dt Time elapsed in seconds
+     */
     virtual void update(bl::engine::Engine& engine, float dt) override;
 
+    /**
+     * @brief Renders the game and hint box
+     *
+     * @param engine The game engine
+     * @param lag Time elapsed not accounted for in update
+     */
     virtual void render(bl::engine::Engine& engine, float lag) override;
 
 private:
