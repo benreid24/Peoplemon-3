421c91c10644add829ccae339eed8a45b017d803
Ben Reid <reidben24@gmail.com>
2022-04-10 14:09:23 -0400

    Add missing methods to battle state

--- src/Core/Battles/BattleState.cpp
+++ src/Core/Battles/BattleState.cpp
@@ -11,11 +11,23 @@ BattleState::BattleState(Stage state)
 , currentMover(0)
 , firstMover(0) {}
 
+void BattleState::beginRound(bool pf) {
+    currentMover = pf ? 0 : 1;
+    firstMover   = currentMover;
+}
+
+BattleState::Stage BattleState::nextTurn() {
+    currentMover = (currentMover + 1) % 2;
+    return currentMover == firstMover ? Stage::WaitingChoices : Stage::TurnStart;
+}
+
 Battler& BattleState::localPlayer() { return player; }
 
 Battler& BattleState::enemy() { return opponent; }
 
 Battler& BattleState::activeBattler() { return currentMover == 0 ? player : opponent; }
+
+Battler& BattleState::inactiveBattler() { return currentMover == 1 ? player : opponent; }
 
 BattleState::Stage BattleState::currentStage() const { return stage; }
 
